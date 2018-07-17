768dc6cd120d85adc146af8ea8377a6dbd646465
AlexKraas <canya.kras.91@gmail.com>
2018-07-17 14:18:37 +0300

    mycommit

---
+++ Magentotutorial/Helloworld/Blocks/View.php
@@ -0,0 +1,4 @@
+<?php
+class Magentotutorial_Helloworld_Block_View extends Mage_Core_Block_Template
+{protected function _toHtml()
+{return "Hello World";}}
---
+++ Magentotutorial/Helloworld/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class Magentotutorial_Helloworld_IndexController extends Mage_Core_Controller_Front_Action 
+{        
+    public function indexAction() 
+	{
+      $this->loadLayout();
+    $this->renderLayout();
+    }
+	
+}
